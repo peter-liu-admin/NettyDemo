d82a691ee457f57a4da96d238046a09a3011134f
lzh <3255635927@qq.com>
2023-10-22 22:14:38 +0800

    完成P22到P23

---
+++ netty-service/src/test/java/com/exp/day/nio/Client.java
@@ -0,0 +1,23 @@
+package com.exp.day.nio;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
+/**
+ * @Author: PeterLiu
+ * @Date: 2023/10/22 21:50
+ * @Description:
+ */
+@Slf4j
+public class Client {
+    public static void main(String[] args) throws IOException {
+        final SocketChannel clientChannel = SocketChannel.open();
+        clientChannel.connect(new InetSocketAddress("localhost",8999));
+        clientChannel.write(Charset.defaultCharset().encode("helloe7777"));
+        log.debug("等待");
+    }
+}
---
+++ netty-service/src/test/java/com/exp/day/nio/Server.java
@@ -0,0 +1,51 @@
+package com.exp.day.nio;
+
+import com.exp.day.utils.ByteBufferUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: PeterLiu
+ * @Date: 2023/10/22 10:40
+ * @Description: 使用NIO理解阻塞模式
+ */
+@Slf4j
+public class Server {
+    public static void main(String[] args) throws IOException {
+        //0、分配内存
+        ByteBuffer buffer = ByteBuffer.allocate(16);
+        //1、创建服务器,打开通道
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        //2、绑定监听的端口
+        ssc.bind(new InetSocketAddress(8999));
+        //3、管道集合
+        List<SocketChannel> channelList = new ArrayList<>();
+        //4、循环遍历
+        while (true) {
+            log.debug("连接中.....");
+            //5、accept 建立与客户端连接， SocketChannel 用来与客户端之间通信。
+            SocketChannel sc = ssc.accept();//阻塞方法，线程停止运行
+            log.debug("已连接---{}", sc);
+            //6、可能不止一个客户端，所以需要加入到集合里面
+            channelList.add(sc);
+            for (SocketChannel socketChannel : channelList) {
+                log.debug("读取数据前:{}",socketChannel);
+                //7、从通道里面读取数据写到缓冲区里面
+                socketChannel.read(buffer);//阻塞方法，线程停止运行
+                //8、切换为读模式
+                buffer.flip();
+                ByteBufferUtil.debugRead(buffer);
+                //9、切换为写模式
+                buffer.clear();
+                log.debug("读取数据后:{}",socketChannel);
+            }
+        }
+    }
+}
